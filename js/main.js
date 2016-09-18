002f3e98fb048fd878f6b33a6905e6ea26e7b9ed
nikyusof <nikyusof@bu.edu>
2016-09-18 05:19:06 -0400

    A good stop for now i think

--- js/main.js
+++ js/main.js
@@ -51,44 +51,62 @@ function initializeTyped($element, textToDisplay) {
 $(document).ready(function() {
   initializeTypedArray();
 
-  var slider = $('.bxslider').bxSlider({
-    speed: 350,
-    nextSelector: '#slider-next',
-    prevSelector: '#slider-prev',
-    nextText: '',
-    prevText: '',
-    pager: false,
-    onSliderLoad: function(currentIndex) {
-      initializeTyped($('.js-slide-one'), typedArray[currentIndex][TYPEDARRAY_TEXT_FIELD], currentIndex);
-      typedArray[currentIndex][TYPEDARRAY_INITIALIZED_FIELD] = true;
-    },
-    onSlideAfter: function(e, oldIndex, newIndex) {
-      var hasInitialized = typedArray[newIndex][TYPEDARRAY_INITIALIZED_FIELD];
-      if (!hasInitialized) {
-        var text = typedArray[newIndex][TYPEDARRAY_TEXT_FIELD];
-        if (newIndex == HOME_SECTION) {
-          initializeTyped($('.js-slide-one'), text);
-        } else if (newIndex == INFO_SECTION) {
-          initializeTyped($('.js-slide-two'), text);
-        }
-        typedArray[newIndex][TYPEDARRAY_INITIALIZED_FIELD] = true;
-      }
+  var fullpage = $('#fullpage').fullpage({
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    sectionsColor : ['#000', 'rgb(220,79,83)', 'rgb(171, 221, 182)', 'rgb(212, 145, 182)', '#2970bc'],
+    anchors: ['slide-one', 'slide-two', 'slide-three', 'slide-four', 'slide-five'],
+    menu: '.menu',
+    afterRender: function () {
+      setInterval(function () {
+        $.fn.fullpage.moveSlideRight();
+      }, 6000);
     }
   });
 
-  $('.js-nav').on('click', function(e, h) {
-    e.preventDefault();
-    slider.goToSlide(e.currentTarget.dataset.index);
+  $('.tlt').typed({
+    strings: ['Satu Hearty.'],
+    showCursor: false
   });
+
+  // var slider = $('.bxslider').bxSlider({
+  //   speed: 350,
+  //   nextSelector: '#slider-next',
+  //   prevSelector: '#slider-prev',
+  //   nextText: '',
+  //   prevText: '',
+  //   pager: false,
+  //   onSliderLoad: function(currentIndex) {
+  //     initializeTyped($('.js-slide-one'), typedArray[currentIndex][TYPEDARRAY_TEXT_FIELD], currentIndex);
+  //     typedArray[currentIndex][TYPEDARRAY_INITIALIZED_FIELD] = true;
+  //   },
+  //   onSlideAfter: function(e, oldIndex, newIndex) {
+  //     var hasInitialized = typedArray[newIndex][TYPEDARRAY_INITIALIZED_FIELD];
+  //     if (!hasInitialized) {
+  //       var text = typedArray[newIndex][TYPEDARRAY_TEXT_FIELD];
+  //       if (newIndex == HOME_SECTION) {
+  //         initializeTyped($('.js-slide-one'), text);
+  //       } else if (newIndex == INFO_SECTION) {
+  //         initializeTyped($('.js-slide-two'), text);
+  //       }
+  //       typedArray[newIndex][TYPEDARRAY_INITIALIZED_FIELD] = true;
+  //     }
+  //   }
+  // });
+
+  // $('.js-nav').on('click', function(e) {
+  //   e.preventDefault();
+  //   fullpage.goToSlide(e.currentTarget.dataset.index);
+  // });
 });
 
 /* Fix the rotation bug on iphoneipad */
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-  var viewportmeta = document.querySelector('meta[name="viewport"]');
-  if (viewportmeta) {
-    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
-    document.body.addEventListener('gesturestart', function () {
-      viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-    }, false);
-  }
-}
+// if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  // var viewportmeta = document.querySelector('meta[name="viewport"]');
+  // if (viewportmeta) {
+  //   viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+  //   document.body.addEventListener('gesturestart', function () {
+  //     viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+  //   }, false);
+  // }
+// }
