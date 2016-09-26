689bf14f66b597dec1c17eecb66bbf57afaddce0
nikyusof <nikyusof@bu.edu>
2016-09-26 00:54:14 -0400

    Some minor changes

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ _gaq.push(['_trackPageview']);
 
 var HOME_SECTION = 1;
 var INFO_SECTION = 2;
-var CONTACT_SECTION = 3;
-var ABOUT_SECTION = 4;
+var CONTACT_SECTION = 4;
+var ABOUT_SECTION = 5;
 
 var TYPEDARRAY_TEXT_FIELD = 'text';
 var TYPEDARRAY_INITIALIZED_FIELD = 'initialized';
@@ -48,6 +48,33 @@ function initializeTyped($element, textToDisplay) {
 
 function initiateAnimation(method, angle) {
   switch (method) {
+    case 'scale':
+      origami.anima({scaleX:0.6, scaleY:0.6}, 400);
+      origami.anima({scale:"1, 1"}, 400);
+      break;
+    case 'rotate':
+      origami.anima({rotate:45}, 400); // deg is default unit
+      origami.anima({rotateX:"0deg"}, 400);
+      break;
+    case 'skew':
+      origami.anima({skewX:20, skewY:20}, 400); // deg is default unit
+      origami.anima({skew:"0deg, 0deg"}, 400);
+      break;
+    case 'transform':
+      origami.css("transform-origin","0% 0%");
+      origami.anima({rotate:"45deg"}, 400);
+      break;
+    case 'flip':
+      origami.anima3d({rotateX:"100deg", rotateY:"100deg", rotateZ:"100deg"}, 400).anima2d({scale:0.6}, 400);
+      origami.anima3d({rotateX:"0deg", rotateY:"0deg", rotateZ:"0deg"}, 400).anima2d({scale:1}, 400);
+      break;
+    case 'no-retain':
+      origami.css("perspective", "100px");
+      origami.parent().css("perspective", "100px");
+      origami.anima({perspective:"100px", rotateX:"0deg", rotateY:"0deg"});
+      origami.anima({perspective:"100px", rotateX:"180deg"}, 400);
+      origami.anima({perspective:"100px", rotateY:"180deg"}, 400);
+      break;
     case 'foldUp':
       origami.foldUp();
       break;
@@ -75,18 +102,23 @@ function initiateAnimation(method, angle) {
   }
 }
 
-function animate() {
-  var methods = ['accordion', 'stairs', 'curl', 'reveal', 'foldUp', 'ramp', 'twist', 'fracture'];
+function animate(type) {
+  var methods = [];
+  if (type === 'oridomi') {
+    methods = ['accordion', 'stairs', 'curl', 'reveal', 'foldUp', 'ramp', 'twist', 'fracture'];
+  } else {
+    methods = ['scale', 'rotate', 'skew', 'transform', 'flip', 'no-retain'];
+  }
   var method = methods[Math.abs(Math.floor(Math.random() * methods.length - Math.random()))];
   var angle = Math.floor(Math.random() * 80 * (Math.random() > .5 ? -1 : 1));
   initiateAnimation(method, angle);
 }
 
-function startAnimate() {
-  animate();
+function startAnimate(type) {
+  animate(type);
   setTimeout(function() {
-    startAnimate();
-  }, 2000);
+    startAnimate(type);
+  }, 3000);
 }
 
 /**
@@ -108,24 +140,45 @@ $(document).ready(function() {
     },
     onLeave: function (index, nextIndex, direction) {
       if (nextIndex == ABOUT_SECTION) {
-        $('.js-follow-us').textillate();
+        // $('.js-about').typed({
+        //   strings: ['Satu Hearty'],
+        //   startDelay: 1000,
+        //   typeSpeed: 5,
+        //   showCursor: false
+        // });
+        // $('.js-about-sub').typed({
+        //   strings: ['Where technology meets love.'],
+        //   startDelay: 3000,
+        //   typeSpeed: 5,
+        //   showCursor: false
+        // });
+      }
+    },
+    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+      if (slideIndex == 1) {
+        $('.js-slide-text').textillate({
+          in: {
+            effect: 'fadeInUpBig'
+          }
+        });
       }
     }
   });
 
-  origami = new OriDomi('.mm', {
-    vPanels: 6,
-    hPanels: 3,
-    ripple: true
-  });
+  var animationType = '';
+  if (navigator.userAgent.indexOf("Chrome") != -1) {
+    origami = new OriDomi('.mm', {
+      vPanels: 6,
+      hPanels: 3,
+      ripple: true
+    });
+    animationType = 'oridomi';
+  } else {
+    origami = $('.mm');
+    animationType = 'anima';
+  }
 
-  startAnimate();
-
-
-  // $('.tlt').typed({
-  //   strings: ['Satu Hearty.'],
-  //   showCursor: false
-  // });
+  startAnimate(animationType);
 
   $('.tlt').textillate();
 });
