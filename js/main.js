b78b682b4e27c3f6813fdd3646460e21210f3ef2
nikyusof <nikyusof@bu.edu>
2016-10-01 23:11:40 -0400

    Some modifications - changing the background image for the slides - create text animations for the slides - increase the font size - change the icon for the menu

--- js/main.js
+++ js/main.js
@@ -15,65 +15,36 @@ _gaq.push(['_trackPageview']);
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 
-var HOME_SECTION = 1;
-var INFO_SECTION = 2;
-var CONTACT_SECTION = 4;
 var ABOUT_SECTION = 5;
-
-var TYPEDARRAY_TEXT_FIELD = 'text';
-var TYPEDARRAY_INITIALIZED_FIELD = 'initialized';
-
-var typedArray = [];
 var origami;
 
-function initializeTypedArrayEntry(index, text) {
-  typedArray[index] = [];
-  typedArray[index][TYPEDARRAY_TEXT_FIELD] = text;
-  typedArray[index][TYPEDARRAY_INITIALIZED_FIELD] = false;
-}
-
-function initializeTypedArray() {
-  initializeTypedArrayEntry(HOME_SECTION, ['Hello World. My name is Nik']);
-  initializeTypedArrayEntry(INFO_SECTION, ['We create beautiful websites']);
-  initializeTypedArrayEntry(CONTACT_SECTION, []);
-  initializeTypedArrayEntry(ABOUT_SECTION, []);
-}
-
-function initializeTyped($element, textToDisplay) {
-  $element.typed({
-    strings: textToDisplay,
-    showCursor: false
-  })
-}
-
+/**
+ * Origami initiation function
+ */
 function initiateAnimation(method, angle) {
   switch (method) {
     case 'scale':
-      origami.anima({scaleX:0.6, scaleY:0.6}, 400);
-      origami.anima({scale:"1, 1"}, 400);
+      origami.anima({scaleX: 0.6, scaleY: 0.6}, 400);
+      origami.anima({scale: '1, 1'}, 400);
       break;
     case 'rotate':
-      origami.anima({rotate:45}, 400); // deg is default unit
-      origami.anima({rotateX:"0deg"}, 400);
+      origami.anima({rotate: 45}, 400); // deg is default unit
+      origami.anima({rotateX: '0deg'}, 400);
       break;
     case 'skew':
-      origami.anima({skewX:20, skewY:20}, 400); // deg is default unit
-      origami.anima({skew:"0deg, 0deg"}, 400);
+      origami.anima({skewX: 20, skewY: 20}, 400); // deg is default unit
+      origami.anima({skew: '0deg, 0deg'}, 400);
       break;
     case 'transform':
-      origami.css("transform-origin","0% 0%");
-      origami.anima({rotate:"45deg"}, 400);
-      break;
-    case 'flip':
-      origami.anima3d({rotateX:"100deg", rotateY:"100deg", rotateZ:"100deg"}, 400).anima2d({scale:0.6}, 400);
-      origami.anima3d({rotateX:"0deg", rotateY:"0deg", rotateZ:"0deg"}, 400).anima2d({scale:1}, 400);
+      origami.css('transform-origin','0% 0%');
+      origami.anima({rotate: '45deg'}, 400);
       break;
     case 'no-retain':
-      origami.css("perspective", "100px");
-      origami.parent().css("perspective", "100px");
-      origami.anima({perspective:"100px", rotateX:"0deg", rotateY:"0deg"});
-      origami.anima({perspective:"100px", rotateX:"180deg"}, 400);
-      origami.anima({perspective:"100px", rotateY:"180deg"}, 400);
+      origami.css('perspective', '100px');
+      origami.parent().css('perspective', '100px');
+      origami.anima({perspective: '100px', rotateX: '0deg', rotateY: '0deg'});
+      origami.anima({perspective: '100px', rotateX: '180deg'}, 400);
+      origami.anima({perspective: '100px', rotateY: '180deg'}, 400);
       break;
     case 'foldUp':
       origami.foldUp();
@@ -102,18 +73,24 @@ function initiateAnimation(method, angle) {
   }
 }
 
+/**
+ * Continuation function for origami animation
+ */
 function animate(type) {
   var methods = [];
   if (type === 'oridomi') {
     methods = ['accordion', 'stairs', 'curl', 'reveal', 'foldUp', 'ramp', 'twist', 'fracture'];
   } else {
-    methods = ['scale', 'rotate', 'skew', 'transform', 'flip', 'no-retain'];
+    methods = ['scale', 'rotate', 'skew', 'transform', 'no-retain'];
   }
   var method = methods[Math.abs(Math.floor(Math.random() * methods.length - Math.random()))];
   var angle = Math.floor(Math.random() * 80 * (Math.random() > .5 ? -1 : 1));
   initiateAnimation(method, angle);
 }
 
+/**
+ * Function to start animate the work section
+ */
 function startAnimate(type) {
   animate(type);
   setTimeout(function() {
@@ -122,12 +99,35 @@ function startAnimate(type) {
 }
 
 /**
+ * Function to start animate the text
+ */
+function initializeTextillate($element) {
+  var transitions = ['flash', 'bounce', 'shake', 'tada', 'swing', 'wobble', 'pulse', 'flip', 'flipInX',
+  'flipInY', 'fadeIn', 'fadeInUp', 'fadeInDown', 'fadeInLeft', 'fadeInRight', 'fadeInUpBig', 'fadeInDownBig',
+  'fadeInLeftBig', 'fadeInRightBig', 'bounceIn', 'bounceInDown', 'bounceInUp', 'bounceInLeft', 'bounceInRight',
+  'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'rollIn'];
+  var transitionEffects = [true, false];
+
+  var transition = transitions[Math.abs(Math.floor(Math.random() * transitions.length - Math.random()))];
+  var text = $element.textillate({
+    in: {
+      effect: transition,
+      sync: transitionEffects[Math.abs(Math.floor(Math.random() * transitions.length - Math.random()))],
+      shuffle: transitionEffects[Math.abs(Math.floor(Math.random() * transitions.length - Math.random()))],
+      reverse: transitionEffects[Math.abs(Math.floor(Math.random() * transitions.length - Math.random()))]
+    }
+  });
+  text.textillate('start');
+}
+
+/**
  * Initialization function
  */
 $(document).ready(function() {
-  initializeTypedArray();
+  var aboutInitialized = false;
 
   var fullpage = $('#fullpage').fullpage({
+    controlArrows: false,
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
     sectionsColor : ['#000', '#000', '#000', '#000', '#000'],
@@ -138,35 +138,44 @@ $(document).ready(function() {
         $.fn.fullpage.moveSlideRight();
       }, 6000);
     },
-    onLeave: function (index, nextIndex, direction) {
-      if (nextIndex == ABOUT_SECTION) {
-        // $('.js-about').typed({
-        //   strings: ['Satu Hearty'],
-        //   startDelay: 1000,
-        //   typeSpeed: 5,
-        //   showCursor: false
-        // });
-        // $('.js-about-sub').typed({
-        //   strings: ['Where technology meets love.'],
-        //   startDelay: 3000,
-        //   typeSpeed: 5,
-        //   showCursor: false
-        // });
+    onLeave: function (index, nextIndex) {
+      if (nextIndex == ABOUT_SECTION && !aboutInitialized) {
+        $('.js-about').typed({
+          strings: ['Satu Hearty'],
+          startDelay: 1000,
+          typeSpeed: 9,
+          showCursor: false
+        });
+        $('.js-about-sub').typed({
+          strings: ['Where technology meets love.'],
+          startDelay: 3000,
+          typeSpeed: 2,
+          showCursor: false
+        });
+        aboutInitialized = true;
       }
     },
-    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-      if (slideIndex == 1) {
-        $('.js-slide-text').textillate({
-          in: {
-            effect: 'fadeInUpBig'
-          }
-        });
+    onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
+      switch (nextSlideIndex) {
+        case 0:
+          initializeTextillate($('.js-slide-text1'));
+          break;
+        case 1:
+          initializeTextillate($('.js-slide-text2'));
+          break;
+        case 2:
+          initializeTextillate($('.js-slide-text3'));
+          break;
+        case 3:
+          initializeTextillate($('.js-slide-text4'));
+          break;
       }
     }
   });
 
   var animationType = '';
-  if (navigator.userAgent.indexOf("Chrome") != -1) {
+  // If this is desktop chrome, use the origami animation
+  if (navigator.userAgent.indexOf('Chrome') != -1) {
     origami = new OriDomi('.mm', {
       vPanels: 6,
       hPanels: 3,
@@ -179,17 +188,4 @@ $(document).ready(function() {
   }
 
   startAnimate(animationType);
-
-  $('.tlt').textillate();
 });
-
-/* Fix the rotation bug on iphoneipad */
-// if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-  // var viewportmeta = document.querySelector('meta[name="viewport"]');
-  // if (viewportmeta) {
-  //   viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
-  //   document.body.addEventListener('gesturestart', function () {
-  //     viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-  //   }, false);
-  // }
-// }
